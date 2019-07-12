d2a42e2fe4c2d5eee85ccaa20af0fbc878ec30e9
skybotz <52811415+skybotz@users.noreply.github.com>
2019-07-12 14:57:01 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'OUI0XpP+Ar3jp40xOQ83UQHU+JmOqSJgv0OrxU3M3fObn4g4+Rmq71UWKkmxbZkaf3eAxSrEE4D0xed666jqqD9OlZ1h+ytyVTJgfmC7k6tasKmbBiQ8UpbFng2CcwYo9W4h1PPyU373u+UTp+v9qgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
